b951ab2ab84312b738f8ba984e3573788a57a3ed
Baksi <myself@baksili.codes>
2023-03-16 05:52:48 +0800

    Update CLI

--- cliai/cli.py
+++ cliai/cli.py
@@ -1,45 +1,57 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
+import sys
+
 import click
 from click_aliases import ClickAliasedGroup
 
 from cliai.core import converse, initiate, manage_config, manage_convos
-import os
 
 
-@click.group(cls=ClickAliasedGroup, invoke_without_command=True)
+@click.group(cls=ClickAliasedGroup)
 def cli():
     pass
 
 
-@cli.command(name='chat', aliases=['converse'])
+@cli.command(name='chat', aliases=['converse'],
+             short_help='Interactive Chat Mode')
 @click.option('--api-key',
               default=lambda: os.environ.get('OPENAI_API_KEY', ''),
+              metavar='<OPENAI_API_KEY>',
               help='Specify an API key')
 @click.option('--api-base',
               default=lambda: os.environ.get('OPENAI_API_BASE', ''),
+              metavar='<OPENAI_API_BASE>',
               help='Use a custom API base')
 @click.option('--verbose', '-v', is_flag=True, help='Turn on verbose output')
 def converse_command(api_key, api_base, verbose):
     """
-    Start conversation
+    Start an interactive conversation.
     """
     initiate(api_key, api_base)
     converse(verbose=verbose)
 
 
-@cli.command(name='config')
+@cli.command(name='config',
+             short_help='Configuration Manager')
 def config_command():
+    """
+    Enter configuration manager.
+    """
     manage_config()
 
 
 def interactive():
     """
-    Enters interactive mode (TUI)
+    Enter interactive mode (TUI).
     """
     pass
 
 
 if __name__ == '__main__':
-    cli()
+    if len(sys.argv) == 1:
+        cli.main(['--help'])
+    else:
+        cli()
